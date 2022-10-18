05540ea212a3279e1ddf6343b42eafb125d12437
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-10-18 12:28:36 -0700

    Update

--- runs-on-emulated.sh
+++ runs-on-emulated.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install -y git build-essential gcc make curl
 
 git clone https://github.com/godaddy/cobhan-rust.git
-cd cobhan-rust || exit
+cd cobhan-rust/cobhan || exit
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 
