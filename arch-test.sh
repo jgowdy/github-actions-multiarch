5b5181413f581c2a641759908466b0980888ebc5
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-10-18 13:46:23 -0700

    Update

--- arch-test.sh
+++ arch-test.sh
@@ -9,10 +9,14 @@ sudo apt-get install qemu binfmt-support qemu-user-static -y
 echo "Register qemu support with docker"
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
+echo "Clone cobhan-rust on host"
 git clone https://github.com/godaddy/cobhan-rust.git
 cd cobhan-rust/cobhan || exit
+echo "Cargo update on host"
 cargo update
+ls -la "${HOME}/.cargo/git"
+ls -la "${HOME}/.cargo/registry"
 cd ../..
 
 echo "Testing ${TEST_ARCH} emulation"
-docker run --rm -v $(pwd):/build    -v "${HOME}/.cargo/git:/usr/local/cargo/git" -v "${HOME}/.cargo/registry:/usr/local/cargo/registry" "${TEST_ARCH}/debian:sid" /build/runs-on-emulated.sh
+docker run --rm -v $(pwd):/build -v "${HOME}/.cargo/git:/root/.cargo/git" -v "${HOME}/.cargo/registry:/root/.cargo/registry" "${TEST_ARCH}/debian:sid" /build/runs-on-emulated.sh
